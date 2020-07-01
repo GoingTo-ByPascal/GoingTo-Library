f29712cd4caac831ec2a34ac40b94fb53d37c600
IPorteniu <lporteniu@gmail.com>
2020-07-01 02:35:52 -0500

    Agregar archivos de proyecto.

---
+++ GoingTo-Library/Observer/Observable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoingTo_Library.Observer
+{
+    public interface IObservable
+    {
+        void RegisterObserver(IObserver Observer);
+        void RemoveObserver(IObserver Observer);
+        void NotifyObservers();
+    }
+}
---
+++ GoingTo-Library/Observer/Observer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoingTo_Library.Observer
+{
+    public interface IObserver
+    {
+        void Update(object Target);
+        void StartListening(IObservable Server);
+        void StopListening();
+    }
+}
